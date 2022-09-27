7b58acd2884627a1168d5aa01b1553615def2107
patelmalav <hey@patelmalav.dev>
2022-09-27 10:37:36 -0400

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# CS-680 Course Work
+To run the project you need to have ant installed in your system, and in path.
+
+To run the project type following in a terminal
+```
+    ant
+
+    # or
+
+    ant -f build.xml
+```
+
+# junit5-jupiter-starter-ant
+
+The `junit5-jupiter-starter-ant` project demonstrates how to execute JUnit Jupiter
+tests using [Apache Ant](https://ant.apache.org/) 1.10.4 or higher.
+
+This sample project does not aim to demonstrate how to use the JUnit Jupiter APIs.
+
+For detailed  information on the JUnit Jupiter programming and extension models,
+please consult the [User Guide](http://junit.org/junit5/docs/current/user-guide/).
---
+++ src/test/edu/umb/cs680/hw01/PrimeGeneratorTest.java
@@ -0,0 +1,90 @@
+package edu.umb.cs680.hw01;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PrimeGeneratorTest {
+
+    @Test
+    public void generatePrimesBetween_1_to_150() {
+
+        PrimeGenerator cut = new PrimeGenerator(1, 150);
+        cut.generatePrimes();
+
+        Long[] result = (Long[]) cut.getPrimes().toArray(new Long[0]);
+
+        Long[] actualPrimesBetween1to150 = { 2l, 3l, 5l, 7l, 11l, 13l,
+                17l, 19l, 23l, 29l, 31l, 37l, 41l, 43l, 47l, 53l, 59l,
+                61l, 67l, 71l, 73l, 79l, 83l, 89l, 97l, 101l, 103l,
+                107l, 109l, 113l, 127l, 131l, 137l, 139l, 149l };
+
+        assertArrayEquals(result, actualPrimesBetween1to150);
+    }
+
+    @Test
+    public void generatePrimesBetween_Minus10_to_10() {
+
+        Long from = -10l;
+        Long to = 10l;
+        try {
+            new PrimeGenerator(from, to);
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Wrong input values: from=" + from + " to=" + to);
+        }
+    }
+
+    @Test
+    public void generatePrimesBetween_1_to_Minus10() {
+        Long from = 1l;
+        Long to = -10l;
+        try {
+            new PrimeGenerator(from, to);
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Wrong input values: from=" + from + " to=" + to);
+        }
+    }
+
+    @Test
+    public void generatePrimesBetween_20_to_1() {
+        Long from = 20l;
+        Long to = 1l;
+        try {
+            new PrimeGenerator(from, to);
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Wrong input values: from=" + from + " to=" + to);
+        }
+    }
+
+    @Test
+    public void generatePrimesBetween_1_to_1() {
+        Long from = 1l;
+        Long to = 1l;
+        try {
+            new PrimeGenerator(from, to);
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Wrong input values: from=" + from + " to=" + to);
+        }
+    }
+
+    @Test
+    public void generatePrimesBetween_12_to_12() {
+        Long from = 12l;
+        Long to = 12l;
+        try {
+            new PrimeGenerator(from, to);
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Wrong input values: from=" + from + " to=" + to);
+        }
+    }
+
+    @Test
+    public void checkIfArrayIsEmptyWhen_genPrimes_MethodIsNotCalled() {
+        PrimeGenerator cut = new PrimeGenerator(1, 150);
+
+        Long[] result = (Long[]) cut.getPrimes().toArray(new Long[0]);
+
+        Long[] actual = {};
+
+        assertArrayEquals(result, actual);
+    }
+}
