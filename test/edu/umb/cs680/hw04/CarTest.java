c6280fb331360383c4dc3691814320b6a8add989
patelmalav <hey@patelmalav.dev>
2022-10-23 16:27:46 -0400

    Updated test-report directory

--- test/edu/umb/cs680/hw04/CarTest.java
+++ test/edu/umb/cs680/hw04/CarTest.java
@@ -14,6 +14,10 @@ public class CarTest {
         String[] expected = { "Tesla", "CyberTruck", "2022" };
         Car actual = new Car("Tesla", "CyberTruck", 2022, 300, 75000.00f);
 
+        Car car1 = new Car("BMW", "x6", 2023, 380, 65000.00f);
+        Car car2 = new Car("BMW", "x6", 2023, 380, 65000.00f);
+
         assertArrayEquals(expected, carToStringArray(actual));
+        assertArrayEquals(carToStringArray(car1), carToStringArray(car2));
     }
-}
+};
